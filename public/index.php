8285bdd36a60a7d0ff87c84ccfbf1b212f63e7ed
Marc Barbeau <hello@marcbarbeau.com>
2016-10-19 16:14:16 -0600

    HTTP Method routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
- include('../app/editor.php');
+
+switch($_SERVER['REQUEST_METHOD'])
+{
+  case 'POST':
+    include('../app/save.php');
+  break;
+
+  case 'GET':
+    include('../app/editor.php');
+  break;
+
+  default:
+    header('HTTP/1.1 405 Method Not Allowed');
+    header('Allow: GET, POST');
+  break;
+}
+
+
 ?>
