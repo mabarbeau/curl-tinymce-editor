7d9d639764ed144a3d2ff6e669a74fec2bcb8245
Marc Barbeau <hello@marcbarbeau.com>
2016-10-19 11:00:47 -0600

    Submit to file

--- submit.php
+++ submit.php
@@ -1,11 +1,16 @@
 <head>
 		<link rel="stylesheet" type="text/css" href='https://flh.fhwa.dot.gov/library/template/style/'>
 		<link  rel="stylesheet" type="text/css" href='https://flh.fhwa.dot.gov/library/template/style/carousel.css'>
-		
+
 </head>
 
 <?php
 //var_dump($_POST);
-echo '<h1>'.  $_POST['URL'] . '#' .  $_POST['ID'] . '</h1>';
-echo $_POST['code'];
+
+$myfile = fopen("submited/newfile.html", "w") or die("Unable to open file!");
+$txt = $_POST['URL'] . '#' .  $_POST['ID'] . "\n";
+fwrite($myfile, $txt);
+fwrite($myfile, $_POST['code']);
+fclose($myfile);
+
 ?>
