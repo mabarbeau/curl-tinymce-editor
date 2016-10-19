1d6ff0f38a4c9e15c3a8b6815ff9ed8f4b2e7f2b
Marc Barbeau <hello@marcbarbeau.com>
2016-10-19 11:20:27 -0600

    Create admin folder to view submited html

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<head>
+	<link rel="stylesheet" type="text/css" href='https://flh.fhwa.dot.gov/library/template/style/'>
+	<link  rel="stylesheet" type="text/css" href='https://flh.fhwa.dot.gov/library/template/style/carousel.css'>
+
+  <title>The Office of Federal Lands Highway (FLH)</title>
+  <script type="text/javascript" src="https://flh.fhwa.dot.gov/library/javascript/jquery.min.js"></script>
+  <script type="text/javascript" src="https://flh.fhwa.dot.gov/library/javascript/jquery.jshowoff.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="https://flh.fhwa.dot.gov/library/template/style/carousel.css" media="all" />
+  <!--[if lt IE 9]><style type="text/css">.jshowoff-controls { bottom: 29px; }</style><![endif]-->
+
+	<link rel="stylesheet" type="text/css" media="screen" href="https://flh.fhwa.dot.gov//library/template/style/" />
+	<link rel="stylesheet" type="text/css" media="print" href="https://flh.fhwa.dot.gov//library/template/style/print.css" />
+</head>
+
+<?php
+  include '../submited/newfile.html';
+?>
