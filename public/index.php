617a502061739739987c355644f092c93c24cdce
Marc Barbeau <hello@marcbarbeau.com>
2016-10-20 12:27:30 -0600

    Fix cookie naming error

--- public/index.php
+++ public/index.php
@@ -5,14 +5,14 @@ switch($_SERVER['REQUEST_METHOD'])
   case 'POST':
     include('../app/save.php');
     header("Location: " . $_SERVER['REQUEST_URI']);
-    setcookie($_SERVER['REQUEST_URI'], '1', time() + (5 * 30), "/"); //5 minutes
+    setcookie('saved', '1', time() + (5 * 30), "/"); //5 minute cookie
     exit();
   break;
 
   default:
-    if(isset($_COOKIE[$_SERVER['REQUEST_URI']])){
+    if(isset($_COOKIE['saved'])){
       echo 'Saved!';
-      setcookie($_SERVER['REQUEST_URI'], "", time() - 3600);
+      setcookie('saved', "0", time() - 3600, "/"); //Delete cookie
     }
 
     include('../app/editor.php');
