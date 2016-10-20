e70bda82e2f39c5c8223730450cbdc2d984ca8cf
Marc Barbeau <hello@marcbarbeau.com>
2016-10-20 11:22:51 -0600

    Remove tinyMCE file toolbar

--- public/js/tinymce.config.js
+++ public/js/tinymce.config.js
@@ -2,14 +2,15 @@ tinymce.init({
   selector: 'textarea',
   height: 600,
   theme: 'modern',
+  removed_menuitems: 'newdocument',
   plugins: [
-  'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+  'advlist autolink lists link image charmap preview hr anchor pagebreak',
   'searchreplace wordcount visualblocks visualchars code fullscreen',
   'insertdatetime media nonbreaking save table contextmenu directionality',
   'emoticons paste textcolor colorpicker textpattern '
   ],
-  toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  toolbar2: 'print preview media | forecolor backcolor emoticons',
+  toolbar1: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  toolbar2: 'preview media | forecolor backcolor',
   image_advtab: true,
   relative_urls: true,
   document_base_url: '//flh.fhwa.dot.gov/',
