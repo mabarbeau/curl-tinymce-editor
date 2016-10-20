2a196b3bab62366a70b8997a3f96e72ced890464
Marc Barbeau <hello@marcbarbeau.com>
2016-10-20 15:28:03 -0600

    A diff implementation for PHP

---
+++ admin/diff.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../app/page.php');
+require_once '../vendor/class.Diff.php';
+require_once('../app/file.php');
+
+$page = new Page('https://flh.fhwa.dot.gov');
+$page->load('/');
+
+$file = new File('test.htm', $page->output());
+$file->save();
+
+// include the Diff class
+echo Diff::toTable(Diff::compareFiles('../submited/index.htm', '../submited/test.htm'));
+
+ ?>
+<style>
+.diff td{
+  vertical-align : top;
+  white-space    : pre;
+  white-space    : pre-wrap;
+  font-family    : monospace;
+}
+</style>
