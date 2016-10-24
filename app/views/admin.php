5a81a43683e900e0935eae2dfae0c836a1f8e178
Marc Barbeau <hello@marcbarbeau.com>
2016-10-24 16:15:42 -0600

    Create table to list pending submits

--- app/views/admin.php
+++ app/views/admin.php
@@ -3,10 +3,32 @@ $dir    = __DIR__ . '/../../submited';
 $files = preg_grep('/^([^.])/', scandir($dir));
 
 ?>
-<ul>
+<table>
   <?php foreach ($files as $file): ?>
-    <li>
-      <?=$file?>
-    </li>
+    <?php
+    $array = explode('.', $file);
+    $last = $array[count($array) - 1];
+    array_pop($array);
+    $string = implode('/', $array);
+    $full = $string .'.' . $last;
+    ?>
+    <tr>
+      <td>
+        <?=$full?>
+      </td>
+      <td>
+        <a href='/editor/<?=$full?>' target="_blank">
+          Edit
+        </a>
+      </td>
+      <td>
+        <a href='/browser/<?=$full?>' target="_blank">
+          View
+        </a>
+      </td>
+      <td>
+          Delete
+      </td>
+    </tr>
   <?php endforeach;?>
-</ul>
+</table>
